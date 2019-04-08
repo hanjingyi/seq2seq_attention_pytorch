a278a063f09e3e6d29001f77ac6bd89ff1158a8f
hanjingyi <jingyii.han@gmail.com>
2019-04-08 17:40:46 +0200

    model structurn - Encoder

---
+++ model.py
@@ -0,0 +1,26 @@
+import math
+import torch
+import random
+from torch import nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+
+
+class Encoder(nn.Module):
+    def __init__(self, input_size, embed_size, hidden_size,
+                 n_layers=1, dropout=0.5):
+        super(Encoder, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.embed_size = embed_size
+        self.embed = nn.Embedding(input_size, embed_size)
+        self.gru = nn.GRU(embed_size, hidden_size, n_layers,
+                          dropout=dropout, bidirectional=True)
+
+    def forward(self, src, hidden=None):
+        embedded = self.embed(src)
+        outputs, hidden = self.gru(embedded, hidden)
+        # sum bidirectional outputs
+        outputs = (outputs[:, :, :self.hidden_size] +
+                   outputs[:, :, self.hidden_size:])
+        return outputs, hidden
