6e3a6f8ade9ea4eda3742a76c1b4d576b9299d67
hanjingyi <jingyii.han@gmail.com>
2019-04-09 17:50:46 +0200

    model structurn - Encoder - Attention. Update utils.py - Add sent2idx().

--- model.py
+++ model.py
@@ -40,40 +40,59 @@ class EncoderAtt(nn.Module):
         return outputs, hidden
 
 
-class DecoderAtt(nn.Module):
-    def __init__(self, embed_size, hidden_size, output_size,n_layers=1, dropout=0.2):
-        super(DecoderAtt, self).__init__()
-        self.embed_size = embed_size
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, embed_size, output_size):
+        super(DecoderRNN, self).__init__()
         self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.n_layers = n_layers
 
-        self.embed = nn.Embedding(output_size, embed_size) # output vocab size
-        self.dropout = nn.Dropout(dropout, inplace=True)
-        self.attention = Attention(hidden_size)
-        self.gru = nn.GRU(hidden_size + embed_size, hidden_size,
-                          n_layers, dropout=dropout)
-        self.out = nn.Linear(hidden_size * 2, output_size)
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(embed_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
 
-    def forward(self, input, last_hidden, encoder_outputs):
-        # Get the embedding of the current input word (last output word)
-        embedded = self.embed(input).unsqueeze(0)  # (1,B,N)
-        embedded = self.dropout(embedded)
-        # Calculate attention weights and apply to encoder outputs
-        attn_weights = self.attention(last_hidden[-1], encoder_outputs)
-        context = attn_weights.bmm(encoder_outputs.transpose(0, 1))  # (B,1,N)
-        context = context.transpose(0, 1)  # (1,B,N)
-        # Combine embedded input word and attended context, run through RNN
-        rnn_input = torch.cat([embedded, context], 2)
-        output, hidden = self.gru(rnn_input, last_hidden)
-        output = output.squeeze(0)  # (1,B,N) -> (B,N)
-        context = context.squeeze(0)
-        output = self.out(torch.cat([output, context], 1))
-        output = F.log_softmax(output, dim=1)
-        return output, hidden, attn_weights
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
 
 
 
+# class DecoderAtt(nn.Module):
+#     def __init__(self, embed_size, hidden_size, output_size,n_layers=1, dropout=0.2):
+#         super(DecoderAtt, self).__init__()
+#         self.embed_size = embed_size
+#         self.hidden_size = hidden_size
+#         self.output_size = output_size
+#         self.n_layers = n_layers
+#
+#         self.embed = nn.Embedding(output_size, embed_size) # output vocab size
+#         self.dropout = nn.Dropout(dropout, inplace=True)
+#         self.attention = Attention(hidden_size)
+#         self.gru = nn.GRU(hidden_size + embed_size, hidden_size,
+#                           n_layers, dropout=dropout)
+#         self.out = nn.Linear(hidden_size * 2, output_size)
+#
+#     def forward(self, input, last_hidden, encoder_outputs):
+#         # Get the embedding of the current input word (last output word)
+#         embedded = self.embed(input).unsqueeze(0)  # (1,B,N)
+#         embedded = self.dropout(embedded)
+#         # Calculate attention weights and apply to encoder outputs
+#         attn_weights = self.attention(last_hidden[-1], encoder_outputs)
+#         context = attn_weights.bmm(encoder_outputs.transpose(0, 1))  # (B,1,N)
+#         context = context.transpose(0, 1)  # (1,B,N)
+#         # Combine embedded input word and attended context, run through RNN
+#         rnn_input = torch.cat([embedded, context], 2)
+#         output, hidden = self.gru(rnn_input, last_hidden)
+#         output = output.squeeze(0)  # (1,B,N) -> (B,N)
+#         context = context.squeeze(0)
+#         output = self.out(torch.cat([output, context], 1))
+#         output = F.log_softmax(output, dim=1)
+#         return output, hidden, attn_weights
 
 
 
