3834c8f50d6ce595a10f4d79f598218e85a29ea6
hanjingyi <jingyii.han@gmail.com>
2019-05-24 17:25:48 +0200

    model structurn - Encoder - Attention. Update utils.py - Add sent2idx().

--- pytorch_notes.py
+++ pytorch_notes.py
@@ -65,3 +65,33 @@ variable.data
 variable.data.numpy()
 
 
+# Activation function
+
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+# fake data
+x=torch.linspace(-5,5,200)
+x=Variable(x)
+y_relu=F.relu(x).data.numpy()
+y_sigmoid=torch.sigmoid(x).data.numpy()
+y_tanh=torch.tanh(x).numpy()
+y_softplus=F.softplus(x).data.numpy()
+
+plt.figure(1,figsize=(8,6))
+plt.subplot(221)
+plt.plot(x_np,y_relu,c='blue',label='relu')
+plt.ylim(-1,5)
+plt.legend(loc='best')
+
+plt.subplot(222)
+plt.plot(x_np,y_sigmoid,c='blue',label='sigmoid')
+plt.ylim((-0.2,1.2))
+plt.legend(loc='best')
+
+plt.subplot(223)
+plt.plot(x_np,y_tanh,c='blue',label='tanh')
+plt.ylim((-1.2,1.2))
+plt.legend(loc='best')
+
